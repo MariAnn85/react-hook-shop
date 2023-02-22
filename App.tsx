9294cb86715e110b9c651a454a0f7710010aacb3
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-22 19:22:04 +0400

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './style.css';
 import React from 'react';
-//import Product from 'src/components/Product;
+import Product from './Product';
 
   function App() {
   return <Product />
