c6dfe6b21b78e788f25290ee8d0772c59b63590c
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-23 21:11:50 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import React from "react";
 import ReactDOM from "react-dom";
 import "style.css";
-import { Route, BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Header from "./header";
 import Footer from "./footer";
