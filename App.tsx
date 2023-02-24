7e71b04b65da1cf128c2ba91c308a49db81961b3
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-24 11:29:27 +0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,33 +1,9 @@
-import * as React from 'react';
-import './style.css';
 import React from 'react';
-import Product from './Product';
+import Product from '../Product/Product';
 
-
-import ReactDOM from "react-dom/client";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Header from "./header";
-import Footer from "./footer";
-
-  function App() {
+function App() {
   return <Product />
 }
 
 export default App;
-export default function App() { 
-return ( 
-<BrowserRouter> 
-<Routes> 
 
-<Route index element={<Product />} /> 
-<Route path="header" element={<Header />} /> 
-<Route path="footer" element={<Footer />} /> 
-
-
-</Routes> 
-</BrowserRouter> 
-); }
-
-const root = ReactDOM.createRoot(document.getElementById('root')); root.render(<App />);
